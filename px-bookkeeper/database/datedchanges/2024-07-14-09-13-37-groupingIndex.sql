157614db97d80e06de3642954d2a362d46d2c241
Stephen Atwell <78044713+stephenatwell@users.noreply.github.com>
2024-07-29 17:02:02 -0700

    Update 2024-07-14-09-13-37-groupingIndex.sql

--- px-bookkeeper/database/datedchanges/2024-07-14-09-13-37-groupingIndex.sql
+++ px-bookkeeper/database/datedchanges/2024-07-14-09-13-37-groupingIndex.sql
@@ -1,16 +1,16 @@
 databaseChangeLog:
-- changeSet:
-    id:  createIndex-groupbyname
-    author:  satwell
-    changes:
-    - createIndex:
-       tableName:  Inventory
-       unique:  false
-       ignore: true
-       indexName: idx_inventory_name_warehousename
-       clustered:  false
-       columns:
-       - column:
-          name:  WarehouseName
-       - column:
-          name:  Name
+  - changeSet:
+      id: createIndex-groupbyname
+      author: satwell
+      changes:
+        - createIndex:
+            tableName: Inventory
+            unique: false
+            ignore: true
+            indexName: idx_inventory_name_warehousename
+            clustered: false
+            columns:
+              - column:
+                  name: WarehouseName
+              - column:
+                  name: Name
